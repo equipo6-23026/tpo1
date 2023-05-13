54d1581ee1b1d29fbcffca834e76f5a1cfa08c85
Cristian <toccicristian@hotmail.com>
2023-05-13 14:21:19 -0300

    crear animacion del menu

--- js/botonmenu.js
+++ js/botonmenu.js
@@ -7,3 +7,24 @@ function menuAbreCierra() {
     }
 }
 
+function menu2AbreCierra(){
+    var menu=document.getElementById("contenedor-enlaces");
+    if((menu.style.display == "none") || (!menu.style.display)){
+        menu.style.display = "block";
+        
+
+
+        document.getElementsByClassName("bar1")[0].setAttribute("transform","translateX(-40px)");
+
+        console.log(document.getElementsByClassName("bar1")[0].getAttribute("transform"));
+
+        document.getElementsByClassName("bar1")[0].setAttribute("background-color","transparent");
+        document.getElementsByClassName("bar2")[0].setAttribute("transform","rotate(45deg)");
+        document.getElementsByClassName("bar3")[0].setAttribute("transform","translateX(40px)");
+        document.getElementsByClassName("bar3")[0].setAttribute("background-color","transparent");
+        
+
+    } else {
+        menu.style.display = "none";
+    }
+}
