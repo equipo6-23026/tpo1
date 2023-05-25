2914727b9fe4e0d8b523002c6dce9e9be04c53d8
toccicristian <toccicristian@hotmail.com>
2023-05-25 18:10:06 -0300

    dar cierre al menu al hacer click en opciones

--- js/botonmenu.js
+++ js/botonmenu.js
@@ -1,8 +1,16 @@
 function menuAbreCierra() {
-    var menu=document.getElementById("contenedor-enlaces");
+    let menu=document.getElementById("contenedor-enlaces");
     if ((menu.style.display == "none") || (!menu.style.display)){
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
     }
 }
+
+for (var s of document.querySelectorAll("a.opcion-menu")){
+    s.addEventListener(
+        "click", function() {
+            document.getElementById("contenedor-enlaces").style.display = "none"}
+    );
+}
+
