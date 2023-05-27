ca9f385da144f327196dc4729877d4cfe6de188a
toccicristian <toccicristian@hotmail.com>
2023-05-27 01:33:21 -0300

    agregar trancision a boton de menu

--- js/botonmenu.js
+++ js/botonmenu.js
@@ -1,15 +1,20 @@
 var boton = document.getElementById("boton-hamburguesa");
 var contenedorEnlaces = document.getElementById("contenedor-enlaces");
-
+var menuIconAbrir  = document.getElementById("icono-menu-abrir");
+var menuIconCerrar = document.getElementById("icono-menu-cerrar");
 
 boton.addEventListener("click", function (){
     if (contenedorEnlaces.classList.contains("contenedor-enlaces--oculto")){
         contenedorEnlaces.classList.remove("contenedor-enlaces--oculto");
+        menuIconAbrir.classList.add("icono-menu-oculto");
+        menuIconCerrar.classList.remove("icono-menu-oculto");        
         setTimeout(function(){
             contenedorEnlaces.classList.remove("contenedor-enlaces--visualmenteoculto");
         }, 20);
     } else {
         contenedorEnlaces.classList.add('contenedor-enlaces--visualmenteoculto');
+        menuIconAbrir.classList.remove("icono-menu-oculto");
+        menuIconCerrar.classList.add("icono-menu-oculto");
         contenedorEnlaces.addEventListener('transitionend', function(e) {
             contenedorEnlaces.classList.add("contenedor-enlaces--oculto");
         }, {
