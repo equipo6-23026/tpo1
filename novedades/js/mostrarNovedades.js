cc9276c3281a2fe5f6921fa1c33adc5ac06c6902
Cristian <toccicristian@hotmail.com>
2023-05-28 02:00:46 -0300

    agregar campo portada a database.json y a novedadesCOntroller

--- novedades/js/mostrarNovedades.js
+++ novedades/js/mostrarNovedades.js
@@ -5,11 +5,15 @@ function novedadesController(movimiento,actual=tituloActual){
         tituloActual=e[0];
         let titulo = document.getElementById('titulo');
         let portada = document.getElementById('portada-highlight');
-/*         console.log(e); */
+        let review = document.getElementById('review');
+
         titulo.innerHTML=`${e[1].titulo}`;
-        
-        /* ACA SE RELLENA NOVEDADES, FALTAN IMAGENES DE JUEGOS PARA EDITAR SRC
-        DE PORTADA */
+        portada.setAttribute("src",`${e[1].portada}`);
+        review.innerHTML=``;
+        for (usuario in e[1].reviews){
+            console.log(`${usuario} : ${e[1].reviews[usuario]}`);
+            review.innerHTML=`${review.innerHTML}<br><span class="username">${usuario}</span> : ${e[1].reviews[usuario]}`
+        }
     })
 }
 window.addEventListener('load',novedadesController(movimiento=0))
