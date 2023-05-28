9af78f1f46d64a96884fed1d1845a578f6ff9fdc
Rodrigo <mendez.r@hotmail.com>
2023-05-27 22:40:49 -0300

    mostrar resultados de busqueda, linkeados a review.html

---
+++ busquedas/js/mostrarReview.js
@@ -0,0 +1,8 @@
+/* const urlSearchParams = new URLSearchParams(window.location.search);
+const reviewTitulo = urlSearchParams.get("titulo"); */
+
+let contenedor = document.getElementById('contenedor-review');
+window.addEventListener('load',()=>{
+/*     contenedor.innerHTML=`${reviewTitulo}: ` */
+    /* TODO importar el objeto review completo para traer las resenas */
+})
